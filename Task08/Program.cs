458e0a6d2b35f4fb30a0f96590f6030dd76778ff
Julian <soodoo129one@example.com>
2023-04-02 17:57:36 +0300

    исправил ошибку в Task08 (указывались цифры от 0 до N)

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 Console.WriteLine("Введите  число ");
 int number1 = Convert.ToInt32(Console.ReadLine());
-int i = 0;
+int i = 1;
 while(i<=number1)
 // пока
 {
-if (0==i%2)
+if (i%2==0)
 {
 Console.Write($"{i},");
 }
